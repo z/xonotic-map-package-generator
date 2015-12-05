fe1ac1635673df90e1a8beb1b1f2dde79fbe7e02
z <z@xnz.me>
2015-12-05 16:25:25 -0500

    first pass on form

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
-    
-    imgData = "R0lGODdhBQAFAIACAAAAAP/eACwAAAAABQAFAAACCIwPkWerClIBADs=";
 
-    var zip = new JSZip();
-    zip.file("Hello.txt", "Hello World\n");
-    var img = zip.folder("images");
-    img.file("smile.gif", imgData, {base64: true});
-    var content = zip.generate({type:"blob"});
-    // see FileSaver.js
-    saveAs(content, "example.zip");
+    $("#generate").click(function(e) {
+        e.preventDefault();
+        imgData = "R0lGODdhBQAFAIACAAAAAP/eACwAAAAABQAFAAACCIwPkWerClIBADs=";
+
+        var zip = new JSZip();
+        zip.file("Hello.txt", "Hello World\n");
+        var img = zip.folder("images");
+        img.file("smile.gif", imgData, {base64: true});
+        var content = zip.generate({type:"blob"});
+        // see FileSaver.js
+        saveAs(content, "example.zip");
+    });
 
 });
