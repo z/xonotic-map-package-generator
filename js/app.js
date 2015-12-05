85f6e1ed3f094e66af28c516a53548775def18a6
z <z@xnz.me>
2015-12-05 16:48:18 -0500

    js gets data from form and can read in map.zip

--- js/app.js
+++ js/app.js
@@ -10,7 +10,31 @@ $(document).ready(function() {
         img.file("smile.gif", imgData, {base64: true});
         var content = zip.generate({type:"blob"});
         // see FileSaver.js
-        saveAs(content, "example.zip");
+        //saveAs(content, "example.zip");
+
+        var bsp, title, description, author, license;
+        bsp = $("#bsp").val();
+        title = $("#title").val();
+        description = $("#description").val();
+        author = $("#author").val();
+        license = $("#license").val();
+
+        var gametypes = [];
+        $("[name=gametypes]:checked").each(function() {
+            gametypes.push($(this).val());
+        });
+
+        console.log(bsp, title, description, author, gametypes, license);
+
+        JSZipUtils.getBinaryContent('template/map.zip', function(err, data) {
+            if(err) {
+                throw err; // or handle err
+            }
+
+            var zip = new JSZip(data);
+            console.log(zip.file("maps/__mapname__.mapinfo").asText());
+        }); 
+
     });
 
 });
